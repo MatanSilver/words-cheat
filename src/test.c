3e58cb5a0ce779ea3b309809c7882975786ea82b
MatanSilver <matansilver@gmail.com>
2018-01-07 12:11:21 +0200

    added unit tests

--- src/test.c
+++ src/test.c
@@ -1,20 +1,17 @@
 #include "test.h"
+#include "words-cheat-c.h"
 
 int main(int argc, char **argv) {
 	assert_err_list_t *assert_err_list = assert_err_list_new();
-	for (int i = 0; i < 12; i++) {
-		char buf[256];
-		sprintf(buf, "%d != %d <-- should never happen", i, i); //should all pass
-		assert_expr(assert_err_list, true, "simple test", buf);
-	}
-	for (int i = 0; i < 257; i++) {
-		char buf[256];
-		sprintf(buf, "%d != %d", i, i+1);
-		assert_expr(assert_err_list, i == i+1, "simple test", buf);
-	}
-	assert_expr(assert_err_list, 1 == 0, "simple test 1", "1 != 0");
-	assert_expr(assert_err_list, 0 == 0, "simple test 2", "0 != 0 ???");
-	assert_expr(assert_err_list, 2 == 1, "simple test 3", "2 != 1");
+	assert_expr(assert_err_list, strchrcnt("I am happy.", 'a') == 2, "strchrcnt test 0", "count mismatch"); 
+	assert_expr(assert_err_list, strchrcnt("I screm loud.", 's') == 1, "strchrcnt test 1", "count mismatch"); 
+	assert_expr(assert_err_list, strchrcnt("Doggo borks.", 'a') == 0, "strchrcnt test 2", "count mismatch"); 
+	
+	assert_expr(assert_err_list, is_possible("whoops", "whoopsie", 4, true), "is_possible test 0", "whoops");
+	assert_expr(assert_err_list, !is_possible("whoops", "whopsie", 4, true), "is_possible test 1", "whoops");
+	assert_expr(assert_err_list, is_possible("whoops", "whopsie", 4, false), "is_possible test 2", "whoops");
+	assert_expr(assert_err_list, !is_possible("bork", "borked", 5, true), "is_possible test 3", "whoops");
+
 	assert_err_list_dump(assert_err_list);
 	assert_err_list_destroy(assert_err_list);
 }
